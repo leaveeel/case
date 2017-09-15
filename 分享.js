9158d74b83466f01f75fe938512b8e4069775c94
leave <leaveeel@gmail.com>
2017-09-15 16:02:07 +0800

    更新了分享js，支持qzone，qq，weibo

--- 分享.js
+++ 分享.js
@@ -1,5 +1,15 @@
 //分享到qq空间 手机端
-function postToQzone(url,title,pic) {
-  var _u = 'https://h5.qzone.qq.com/q/qzs/open/connect/widget/mobile/qzshare/index.html?page=' + url + '&loginpage=&logintype=qzone&title=' + title + '&url=' + url + '&imageUrl=' + pic ;
-  window.open(_u);
+function shareTo(url,title,pic) {
+    if($(this).hasClass("wb")){
+        var sharesinastring='http://v.t.sina.com.cn/share/share.php?title='+title+'&url='+url+'&content=utf-8&sourceUrl=&pic='+pic;
+        window.open(sharesinastring,'newwindow','height=400,width=400,top=100,left=100');
+    }
+    if($(this).hasClass("qq")){
+        var sharesinastring='http://connect.qq.com/widget/shareqq/index.html?title='+title+'&url='+url+'&content=utf-8&sourceUrl=&pics='+pic;
+        window.open(sharesinastring, '分享到QQ好友', 'height=400,width=400,top=100,left=100');
+    }
+    if($(this).hasClass("qz")){
+    	var shareqqzonestring='http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary='+title+'&title=刮刮乐活动&url='+url+'&pic='+pic;
+        window.open(shareqqzonestring,'newwindow','height=400,width=400,top=100,left=100');
+    }
 }
