7995d7f699a759f4349fd8a0265cc6c40b87da23
leave <leaveeel@gmail.com>
2018-01-12 17:14:04 +0800

    修改获取屏幕高度的写法

--- 页面滚动到底部加载.js
+++ 页面滚动到底部加载.js
@@ -1,7 +1,7 @@
 $(window).scroll(function(){  
     var viewH =$("body").height(),//文档高度
         scrollTop =$(window).scrollTop();
-    if(viewH-scrollTop<=$(window).height()+200){//增减 200 控制加载时的高度
+    if(viewH-scrollTop<=window.screen.height+200){//屏幕高度增加 200 控制加载时的位置
         $("body").height("+=200px");
     }  
 });
