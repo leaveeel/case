a5c0d22e9ef65ce7bad21e8242ae54aba4412b90
Li Kai <leaveeel@gmail.com>
2018-10-25 10:55:34 +0800

    数组操作
    数组操作

--- Arr.js
+++ Arr.js
@@ -1,12 +1,12 @@
 function concat(){
-	const arr1=[1,2,3]
-	const arr2=[4,5]
-	const arr3=arr1.concat(arr2)
+    const arr1=[1,2,3]
+    const arr2=[4,5]
+    const arr3=arr1.concat(arr2)
     console.log(arr1)
     console.log(arr3)
 }
 function join(){
-	const arr=[2,3,4]
+    const arr=[2,3,4]
     console.log(arr.join())
     console.log(arr)
 }
